9b69b7332ae2cc0b247afcc22bd15af4612978c7
Bhanu Pratap Singh <nikrcnkhil@gmail.com>
2025-03-08 14:33:10 +0530

    build created

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { connectDB } from './lib/db.js';
-import MedicineRoute from './routes/route.medicine.js';
-import EmergencyRoute from './routes/route.emergencyRequest.js';
-import RareInjectionRoute from './routes/routes.rareInjection.js';
-import cors from 'cors';
-import path, { dirname } from 'path';
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+import MedicineRoute from "./routes/route.medicine.js";
+import EmergencyRoute from "./routes/route.emergencyRequest.js";
+import RareInjectionRoute from "./routes/routes.rareInjection.js";
+import cors from "cors";
+import path from "path";
 
 const app = express();
 
@@ -18,29 +18,25 @@ connectDB();
 
 // Middleware
 app.use(
-    cors({
-        origin: "http://localhost:5173",
-    })
+  cors({
+    origin: "http://localhost:5173",
+  })
 );
-app.use(express.json({ limit: '20mb' }));
+app.use(express.json({ limit: "20mb" }));
 
 // Routes
-app.use('/api/medicine', MedicineRoute);
-app.use('/api/emergency', EmergencyRoute);
-app.use('/api/rareInjection', RareInjectionRoute);
-
+app.use("/api/medicine", MedicineRoute);
+app.use("/api/emergency", EmergencyRoute);
+app.use("/api/rareInjection", RareInjectionRoute);
 
 const __dirname = path.resolve();
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    });
 }
 
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
-});
-
-
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
