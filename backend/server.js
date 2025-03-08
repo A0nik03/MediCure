a6fa09c99d8a1d3d5f46b1860d1a9af482002c6c
Bhanu Pratap Singh <nikrcnkhil@gmail.com>
2025-03-08 14:01:46 +0530

    build created

--- backend/server.js
+++ backend/server.js
@@ -5,30 +5,39 @@ import MedicineRoute from './routes/route.medicine.js'
 import EmergencyRoute from './routes/route.emergencyRequest.js'
 import RareInjectionRoute from './routes/routes.rareInjection.js'
 import cors from 'cors';
+import path from 'path'
 
 const app = express();
 
 // Loading environment variables
 dotenv.config();
-
 const PORT = process.env.PORT;
+
 
 // Connect to MongoDB
 connectDB();
 
+const __dirname = path.resolve();
+
 // Middleware
-app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "http://locahost:5173"
+}));
+app.use(express.json({limit: '20mb'}));
 
 // Routes
-app.get('/', (req, res) => {
-    res.send('Server is Running...');
-})
 
 app.use('/api/medicine', MedicineRoute);
 app.use('/api/emergency', EmergencyRoute);
 app.use('/api/rareInjection', RareInjectionRoute);
 
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '../frontend/dist')))
+}
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
